64de24ae8c6eb368f58d3464bb3a8eb1d7a66071
RajsundarCogent <103045368+RajsundarCogent@users.noreply.github.com>
2022-09-06 19:54:34 +0530

    commit
    commit

--- script.js
+++ script.js
@@ -21,14 +21,7 @@ window.onmousemove = (e) =>{
     cursor2.style.left = e.pageX + 'px';
 }
 
-function passCheck(){
-    
-    if(prompt("Please enter your password","") == "1234"){
-        alert("success")
-    }else{
-        window.location="index.html"
-    }
-  }
+
 
 document.querySelectorAll('a').forEach(links =>{
 
